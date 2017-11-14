225032ffc756e4c048cefa23a8562ec5d5b24943
cr4y <kris@krej.net>
2017-11-14 18:52:56 +0100

    Water

---
+++ Unity-AGH-2017-11/Assets/WaterLevelIncreaser.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class WaterLevelIncreaser : MonoBehaviour {
+    [Range(0,100f)]public float velocity = 10;
+	void Update () {
+        transform.position = new Vector3(0, transform.position.y + Time.deltaTime * velocity/100f, 0);
+	}
+}
